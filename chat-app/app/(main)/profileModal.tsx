3740399d152c2855c55bb219310c16c893d3cd5b
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2025-11-02 14:42:14 +0530

    Implement profile modal with user data display and logout functionality; enhance Header component for better layout

--- chat-app/app/(main)/profileModal.tsx
+++ chat-app/app/(main)/profileModal.tsx
@@ -1,13 +1,155 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  Alert,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import { colors, radius, spacingX, spacingY } from "@/constants/theme";
 import { scale, verticalScale } from "@/utils/styling";
 import ScreenWrapper from "@/components/ScreenWrapper";
+import Header from "@/components/Header";
+import * as Icons from "phosphor-react-native";
+import BackButton from "@/components/BackButton";
+import Avatar from "@/components/Avatar";
+import { useAuth } from "@/contexts/authContext";
+import { UserDataProps } from "@/types";
+import Typo from "@/components/Typo";
+import Input from "@/components/Input";
+import Button from "@/components/Button";
+import { router } from "expo-router";
 
 const ProfileModal = () => {
+  const { user, signOut } = useAuth();
+  const [loading, setLoading] = useState(false);
+  const [userData, setUserData] = useState<UserDataProps>({
+    name: "",
+    email: "",
+    avatar: null,
+  });
+
+  useEffect(() => {
+    if (user) {
+      setUserData({
+        name: user?.name || "",
+        email: user?.email || "",
+        avatar: user?.avatar || null,
+      });
+    }
+  }, [user]);
+
+  const showLogoutAlert = () => {
+    Alert.alert("Confirm", "Are you sure you want to logout?", [
+      {
+        text: "Cancel",
+        style: "cancel",
+        onPress: () => console.log("cance logout"),
+      },
+      { text: "Logout", style: "destructive", onPress: () => handleLogout() },
+    ]);
+  };
+
+  const handleLogout = async () => {
+    router.back();
+    await signOut();
+  };
+
+  const handleSubmit = () => {
+    const { name, avatar } = userData;
+    if (!name.trim()) {
+      Alert.alert("Username is required", "Please enter your username");
+      return;
+    }
+    
+  };
+
   return (
     <ScreenWrapper isModal={true}>
-      <Text>ProfileModal</Text>
+      <View style={styles.container}>
+        <Header
+          title="Update Profile"
+          leftIcon={
+            Platform.OS === "android" && <BackButton color={colors.black} />
+          }
+          style={{ marginVertical: spacingY._15 }}
+        />
+
+        {/* Form */}
+        <ScrollView contentContainerStyle={styles.form}>
+          <View style={styles.avatarContainer}>
+            <Avatar uri={null} size={170} />
+
+            <TouchableOpacity style={styles.editIcon} onPress={() => {}}>
+              <Icons.PencilIcon
+                size={verticalScale(20)}
+                color={colors.neutral800}
+              />
+            </TouchableOpacity>
+          </View>
+
+          <View style={{ gap: spacingY._20 }}>
+            <View style={styles.inputContainer}>
+              <Typo style={{ paddingLeft: spacingX._10 }}>Email</Typo>
+              <Input
+                value={userData.email}
+                containerStyle={{
+                  borderColor: colors.neutral350,
+                  paddingLeft: spacingX._20,
+                  backgroundColor: colors.neutral300,
+                }}
+                onChangeText={(text) =>
+                  setUserData({ ...userData, email: text })
+                }
+                editable={false}
+              />
+            </View>
+
+            <View style={styles.inputContainer}>
+              <Typo style={{ paddingLeft: spacingX._10 }}>Name</Typo>
+              <Input
+                value={userData.name}
+                containerStyle={{
+                  borderColor: colors.neutral350,
+                  paddingLeft: spacingX._20,
+                  // backgroundColor: colors.neutral300,
+                }}
+                onChangeText={(text) =>
+                  setUserData({ ...userData, name: text })
+                }
+                // editable={false}
+              />
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+
+      <View style={styles.footer}>
+        {!loading && (
+          <Button
+            style={{
+              backgroundColor: colors.rose,
+              height: verticalScale(56),
+              width: verticalScale(56),
+            }}
+            onPress={showLogoutAlert}
+          >
+            <Icons.SignOutIcon
+              size={verticalScale(30)}
+              color={colors.white}
+              weight="bold"
+            />
+          </Button>
+        )}
+
+        <Button style={{ flex: 1 }} onPress={handleSubmit} loading={loading}>
+          <Typo color={colors.black} fontWeight={"700"}>
+            Update
+          </Typo>
+        </Button>
+      </View>
     </ScreenWrapper>
   );
 };
@@ -25,10 +167,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     flexDirection: "row",
     justifyContent: "center",
+    paddingHorizontal: spacingX._20,
     gap: scale(12),
     paddingTop: spacingY._15,
     borderTopColor: colors.neutral200,
-    marginBottom: spacingY._10,
+    marginBottom: spacingY._20,
     borderTopWidth: 1,
   },
   form: {
